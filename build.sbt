1288af3ba6966fc26e7e7a68214658e918c6bdeb
ari1021 <o.pe.3.vndt.ku@gmail.com>
2021-11-04 23:47:33 +0900

    feat: add MonadTransformer Example

--- build.sbt
+++ build.sbt
@@ -1,14 +1,15 @@
 import Dependencies._
 
-ThisBuild / scalaVersion     := "2.13.6"
-ThisBuild / version          := "0.1.0-SNAPSHOT"
-ThisBuild / organization     := "com.example"
+ThisBuild / scalaVersion := "2.13.6"
+ThisBuild / version := "0.1.0-SNAPSHOT"
+ThisBuild / organization := "com.example"
 ThisBuild / organizationName := "example"
 
 lazy val root = (project in file("."))
   .settings(
     name := "Introduction-to-Monad-MonaTra-Eff",
-    libraryDependencies += scalaTest % Test
+    libraryDependencies += scalaTest % Test,
+    libraryDependencies += "org.atnos" %% "eff" % "5.21.0"
   )
 
 // See https://www.scala-sbt.org/1.x/docs/Using-Sonatype.html for instructions on how to publish to Sonatype.
---
+++ src/main/scala/example/MonadTransformer.scala
@@ -0,0 +1,42 @@
+package example
+
+import cats.data.EitherT
+import cats.data.OptionT
+
+object MonadTransformer {
+  def twoMonadTransform(): Unit = {
+    val ma: Option[Int] = Some(1)
+    val mb: Either[String, Int] = Right(2)
+
+    // Option[Either[String, Int]]
+    val res: EitherT[Option, String, Int] = for {
+      a <- EitherT.liftF(ma)
+      b <- EitherT.fromEither[Option](mb)
+    } yield a + b
+
+    println(res)
+  }
+
+  def threeMonadTransform(): Unit = {
+    val ma: Option[Int] = Some(1)
+    val mb: Either[String, Int] = Right(2)
+    val mc: List[Int] = List(3, 4)
+
+    // List[Either[String, A]]
+    type EitherTListStringOrA[A] = EitherT[List, String, A]
+    // List[Either[String, Option[Int]]]
+    val res: OptionT[EitherTListStringOrA, Int] = for {
+      a <- OptionT.fromOption[EitherTListStringOrA](ma)
+      b <- {
+        val le: EitherTListStringOrA[Int] = EitherT.fromEither[List](mb)
+        OptionT.liftF(le)
+      }
+      c <- {
+        val le: EitherTListStringOrA[Int] = EitherT.liftF(mc)
+        OptionT.liftF(le)
+      }
+    } yield a + b + c
+
+    println(res)
+  }
+}
